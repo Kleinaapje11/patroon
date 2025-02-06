12c05ac322b0216d63e42bb36079d7af24b53627
Kleinaapje11 <162138282+Kleinaapje11@users.noreply.github.com>
2025-02-06 09:35:06 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+led.plot(4, 0)
+basic.pause(200)
+led.unplot(4, 0)
+basic.pause(200)
+led.plot(3, 0)
+basic.pause(200)
+led.plot(3, 0)
+led.plot(4, 0)
+basic.forever(function () {
+	
+})
